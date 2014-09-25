8fae0ca55a3c00bc5a39df81e4b4ed771ccfadb9
anurag <anurag3rdsep@gmail.com>
2014-09-25 11:32:06 +0530

    replaced old files

--- static/custom.js
+++ static/custom.js
@@ -1,4 +1,4 @@
-var demo = angular.module('demo',['ngRoute']);
+var demo = angular.module('demo',['ngRoute', 'controllers']);
 
 demo.config(function($routeProvider){
 
@@ -13,31 +13,3 @@ demo.config(function($routeProvider){
 	})
 });
 
-demo.controller('appctrl', ['$scope', function(a){
-		a.categories = [
-			{'id':0, 'name': 'development'},
-			{'id':1, 'name': 'design'},
-			{'id':2, 'name': 'exercise'},
-			{'id':3, 'name': 'humor'}
-
-		];
-
-		a.notes = [
-			{'id':0, 'title': 'angularjs', 'body': 'learning new things about angularjs today', 'category': 'development'},
-			{'id':1, 'title': 'photoshop', 'body': 'i had learned PS in my 1st year', 'category': 'design'},
-			{'id':2, 'title': 'gym', 'body': 'i like going to gym, thats it', 'category': 'exercise'},
-			{'id':3, 'title': 'jokes', 'body': 'radio mirchi shows some really pathetic jokes', 'category': 'humor'},
-		];
-
-		a.currentcat = null;
-		a.cat = {};
-
-		function setcurrentcat(category){
-			a.currentcat = category;
-			a.cat.category = category.name;
-			console.log(a.cat.category)
-		}
-
-		a.setcurrentcat = setcurrentcat;
-
-	}])
